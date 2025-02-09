fb13ae055c0746b5d893fca091b0f1daceb4dd01
Himesh Jha <himeshjha22@gmail.com>
2025-02-09 16:11:48 +0530

    controls the output of evm based on input mode(polling/counting)

--- modeControl.v
+++ modeControl.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/27/2020 07:59:45 PM
-// Design Name: 
-// Module Name: modeControl
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module modeControl(
 input clock,
 input reset,
